168e2fdd282fa1a327e1e16dfe7560769965234d
ravinduiddamalgoda <ravinduiddamalgoda55@gmail.com>
2025-04-23 20:12:33 +0530

    application properties and other

---
+++ src/main/java/com/linkup/app/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.linkup.app.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("user/")
+public class UserController {
+
+    @GetMapping("/")
+    String getHello() {
+        return "Hello";
+    }
+
+    
+}
